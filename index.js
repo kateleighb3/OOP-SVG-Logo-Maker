cf53b3ace013371772faa6f71d6e8c5b81d21874
kateleighb3 <kbaldw3@gmail.com>
2023-07-26 16:51:40 -0400

    revise index.js and shapes.js

--- index.js
+++ index.js
@@ -2,39 +2,57 @@ const inquirer = require("inquirer");
 const fs = require("fs"); 
 const {Circle, Square, Triangle} = require("./lib/shapes");
 
-const questions = [
-    {
-        type: "input",
-        name: "text",
-        message: "Enter text up to 3 characters:",
-    },
-    {
-        type: "input",
-        name: "text-color",
-        message: "Enter a color:",
-    },
-    {
-        type: "input",
-        name: "shape-color",
-        message: "Enter a color:",
-    },
-    {
-        type: "list",
-        name: "shape-type",
-        message: "Choose a shape:",
-        choices: ["Circle", "Square", "Triangle"],
-    },
-];
 
+async function generateSVGLogo() { //tell function to wait while user inputs answers
+    try{
+        const userInput = await inquirer.prompt([ 
+            {
+                type: "input",
+                name: "text",
+                message: "Enter text up to 3 characters:",
+            },
+            {
+                type: "input",
+                name: "textColor",
+                message: "Enter a font color:",
+            },
+            {
+                type: "input",
+                name: "shapeColor",
+                message: "Enter a color for the shape:",
+            },
+            {
+                type: "list",
+                name: "shapeType",
+                message: "Choose a shape:",
+                choices: ["Circle", "Square", "Triangle"],
+            },
+        ]);
 
-function init() {
-    inquirer.prompt(questions).then((answers) => {
-        let genMarkdown = generateMarkdown(answers);
-    fs.writeFile('readme.md', genMarkdown, (err) => 
-    err ? console.log(err) : console.log('Successfully created readme.md!')
-    );
-    });
-};
+        //then
+    let shape;
+    switch (userInput.shapeType) {
+        case 'Circle':
+            shape = new Circle(userInput.shapeColor, userInput.textColor, userInput.text);
+            break;
+        case 'Triangle':
+            shape = new Triangle(userInput.shapeColor, userInput.textColor, userInput.text);
+            break;
+    
+        case 'Square':
+            shape = new Square(userInput.shapeColor, userInput.textColor, userInput.text);
+            break;
+        }
+        //then
+    const svgLogo = shape.render();
+//then
+    fs.writeFile("logo.svg", svgLogo);
 
+    //then
+    console.log("Generated logo.svg");
+    } catch (error) {
+        console.log("Error generating logo");
+    }
+}
 
-init();
+generateSVGLogo()
