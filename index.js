5c2e0edf25825f784602f30f4c0a0eb84f67751f
kateleighb3 <kbaldw3@gmail.com>
2023-07-27 21:18:56 -0400

    generate logo

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer");
 const fs = require("fs"); 
+const path = require("path");
 const {Circle, Square, Triangle} = require("./lib/shapes");
 
 
@@ -31,7 +32,7 @@ async function generateSVGLogo() { //tell function to wait while user inputs ans
 
         //then
     let shape;
-    switch (userInput.shapeType) {
+    switch (userInput.shapeType) { // switch = shorthand for if
         case 'Circle':
             shape = new Circle(userInput.shapeColor, userInput.textColor, userInput.text);
             break;
@@ -45,14 +46,17 @@ async function generateSVGLogo() { //tell function to wait while user inputs ans
         }
         //then
     const svgLogo = shape.render();
+    console.log("SVG Logo content:");
+    console.log(svgLogo);
 //then
-    fs.writeFile("logo.svg", svgLogo);
+const logoFilePath = path.join(__dirname, 'logo.svg');
+   fs.writeFileSync(logoFilePath, svgLogo);
 
     //then
     console.log("Generated logo.svg");
     } catch (error) {
         console.log("Error generating logo");
     }
-}
+};
 
-generateSVGLogo()
+generateSVGLogo();
